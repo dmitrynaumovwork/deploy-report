e3111c98f225c943321de3985cbdfa3fcc1ba2b1
Dmytro Naumov <13536743+dmitrynaumovwork@users.noreply.github.com>
2023-12-14 15:52:09 +0200

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -21,6 +21,6 @@ test('get started link', async ({ page }) => {
 test('fails always', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Expect a title "to contain" a substring. It will fail
+  await expect(page).toHaveTitle(/should-always-fail/);
 });
